fefc07a5a6abd894e27882df16ead6235879fb94
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-16 23:26:42 -0300

    creation of middleware for error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { AppError } from '@shared/errors/AppError';
 import { routes } from './routes';
 
 const app = express();
@@ -8,6 +9,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+// middleware para capturar erros
+app.use((error: Error, req: Request, res: Response) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('server is runing');
