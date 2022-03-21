789993aec6554ff0255651ebec8b760097e89888
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-21 15:12:42 -0300

    create product show  service

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { Product } from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface Irequest {
+  id: string;
+}
+
+export class ShowProductsService {
+  public async execute({ id }: Irequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
