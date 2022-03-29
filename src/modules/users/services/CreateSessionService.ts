fbffa66a71e07553ef89c1522c371bf932cd3b9e
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-28 23:36:45 -0300

    add jwt to session creation service

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,6 +1,7 @@
 import { AppError } from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { User } from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 
@@ -9,10 +10,15 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: {};
+  token: string;
+}
+
 export class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
-    const user = await usersRepository.findByEmail(email);
+    const user: User | undefined = await usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email or password', 401);
@@ -24,6 +30,18 @@ export class CreateSessionService {
       throw new AppError('Incorrect email or password', 401);
     }
 
-    return user;
+    const token = sign({}, String(process.env.SIGNATURE_TOKEN), {
+      subject: user.id,
+      expiresIn: '7d',
+    });
+
+    return {
+      user: {
+        user_id: user.id,
+        user_name: user.name,
+        user_avatar: user.avatar,
+      },
+      token,
+    };
   }
 }
