73dbcc40fa5b27432191145c1937ca9475adfa5e
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-28 22:27:02 -0300

    fix: async method without await

--- src/modules/users/controllers/UserSessionController.ts
+++ src/modules/users/controllers/UserSessionController.ts
@@ -5,7 +5,7 @@ export class UserSessionController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
     const createSession = new CreateSessionService();
-    const user = createSession.execute({ email, password });
+    const user = await createSession.execute({ email, password });
     return res.json(user);
   }
 }
