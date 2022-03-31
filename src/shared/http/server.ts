ada1296d17448c82e8511777975088112fd89152
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-30 23:29:46 -0300

    create static route to download files

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { AppError } from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 import { errors } from 'celebrate';
 import { routes } from './routes';
 import '@shared/typeorm';
@@ -14,6 +15,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
