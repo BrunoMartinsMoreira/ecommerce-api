815ca56d5e16f2fc53839ac32624e44eff4b54bb
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-30 23:01:48 -0300

    create user avatar upload controller

---
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { UpadateUserAvatarService } from '../services/UpdateUserAvatarService';
+
+export class UserAvatarController {
+  public async update(req: Request, res: Response): Promise<Response> {
+    const updateAvatar = new UpadateUserAvatarService();
+
+    const user = updateAvatar.execute({
+      userId: req.user.id,
+      avatarFileName: req.file?.filename as string, // coloquei isso aqui pq tava dando erro, funcionou nao sei pq,
+    });
+
+    return res.json(user);
+  }
+}
