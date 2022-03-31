20278ed5bdbee14aa879e98e4b5749eaa6f749e7
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-30 22:44:02 -0300

    create user avatar upload service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+import { User } from '../typeorm/entities/User';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  userId: string;
+  avatarFileName: string;
+}
+
+export class UpadateUserAvatarService {
+  public async execute({ userId, avatarFileName }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    // verifica se o usuário já tem um avatar
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
