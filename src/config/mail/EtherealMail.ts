fb015dd69644681e4b06c9cadac86fc40eaff62e
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-04-16 18:15:18 -0300

    feat: config handlebars mail template

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,12 +1,33 @@
 import nodemailer from 'nodemailer';
+import { HandlebarsMailTemplate } from './HandleBarsMailTemplate';
 
-interface IsendeMail {
-  to: string;
-  body: string;
+interface IMailContact {
+  name: string;
+  email: string;
+}
+interface ITemplateVars {
+  [key: string]: string | number;
+}
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVars;
+}
+interface ISendMail {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
 }
 
 export class EtherealMail {
-  static async sendMail({ to, body }: IsendeMail): Promise<void> {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const mailTemplate = new HandlebarsMailTemplate();
+
     const account = await nodemailer.createTestAccount();
 
     const transporter = nodemailer.createTransport({
@@ -20,10 +41,16 @@ export class EtherealMail {
     });
 
     const message = await transporter.sendMail({
-      from: 'bruno_moreira@fakeapi.teste',
-      to,
-      subject: 'Teste de email fake',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe Bruno Moreira comedor de casada',
+        address: from?.email || 'bruno_moreira@fakeapi.teste',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parser(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
