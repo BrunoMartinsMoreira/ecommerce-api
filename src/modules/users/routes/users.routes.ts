0863bc5c1fe4b3e24eddf3c7b7c0ead026420a26
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-04-20 22:36:54 -0300

    feat: create profile routes

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -3,12 +3,14 @@ import { celebrate, Joi, Segments } from 'celebrate';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 import { UsersController } from '../controllers/UsersController';
+import { UserProfileController } from '../controllers/UserProfileController';
 import { UserAvatarController } from '../controllers/UserAvatarController';
 import { isAuthenticated } from '../../../shared/http/middlewares/isAuthenticated';
 
 const usersRouter = Router();
 const userController = new UsersController();
 const avatarController = new UserAvatarController();
+const profileController = new UserProfileController();
 
 const upload = multer(uploadConfig);
 
@@ -33,3 +35,25 @@ usersRouter.patch(
   avatarController.update,
 );
 export { usersRouter };
+
+usersRouter.get('/profile', isAuthenticated, profileController.show);
+
+usersRouter.put(
+  '/profile',
+  isAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileController.update,
+);
