ce32396feeecd46a3d362e8efd8efa69b2e4d908
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-04-21 22:48:38 -0300

    feat: create customers routes

---
+++ src/modules/customers/routes/customer.routes.ts
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { CustomersController } from '../controllers/CustomersController';
+
+const customerRouter = Router();
+const customersController = new CustomersController();
+
+customerRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+    },
+  }),
+  customersController.create,
+);
+
+customerRouter.get('/', customersController.index);
+
+customerRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.show,
+);
+
+customerRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.update,
+);
+
+customerRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.delete,
+);
+
+export { customerRouter };
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import { productsRouter } from '@modules/products/routes/products.routes';
 import { usersRouter } from '@modules/users/routes/users.routes';
 import { sessionsRouter } from '@modules/users/routes/sessions.routes';
 import { forgotPasswordRouter } from '@modules/users/routes/password.routes';
+import { customerRouter } from '@modules/customers/routes/customer.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', forgotPasswordRouter);
+routes.use('/customers', customerRouter);
 
 export { routes };
