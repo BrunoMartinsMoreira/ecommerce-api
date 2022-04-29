fb01410e204d634d67c9fdd7ac2b5c7009971029
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-04-28 22:10:14 -0300

    feat: created foreign key OrdersProductsOrder

---
+++ src/shared/typeorm/migrations/1651194181331-AddOrdersIdToOrdersProducts.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddOrdersIdToOrdersProducts1651194181331
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsOrder',
+        columnNames: ['order_id'],
+        referencedTableName: 'orders',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // nome da tabela, nome da foreign key
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductsOrder');
+    // nome da tabela, campo a ser deletado
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
