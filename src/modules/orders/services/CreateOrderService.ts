780b2cc0e8fc1c2485797a34cf2ceabd86f59ff9
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-05-03 22:43:35 -0300

    feat: create order service

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,4 +1,3 @@
-import { Customer } from '@modules/customers/typeorm/entities/Customer';
 import { CustomersRepository } from '@modules/customers/typeorm/repositories/CustomersRepository';
 import { ProductsRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
 import { AppError } from '@shared/errors/AppError';
