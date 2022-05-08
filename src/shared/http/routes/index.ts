86fe0aa8f41f1f5d4b1232df153f3c59b1a93827
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-05-08 11:18:27 -0300

    feat: create orders routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import { usersRouter } from '@modules/users/routes/users.routes';
 import { sessionsRouter } from '@modules/users/routes/sessions.routes';
 import { forgotPasswordRouter } from '@modules/users/routes/password.routes';
 import { customerRouter } from '@modules/customers/routes/customer.routes';
+import { ordersRouter } from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', forgotPasswordRouter);
 routes.use('/customers', customerRouter);
+routes.use('/orders', ordersRouter);
 
 export { routes };
