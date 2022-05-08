66b9d2f1a78da5f19df642b58b5b5fa0c70d4651
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-05-08 12:35:33 -0300

    feat: add pagination with typeorm pagination

--- src/modules/customers/services/ListCustomersService.ts
+++ src/modules/customers/services/ListCustomersService.ts
@@ -3,16 +3,28 @@ import { getCustomRepository } from 'typeorm';
 import { Customer } from '../typeorm/entities/Customer';
 import { CustomersRepository } from '../typeorm/repositories/CustomersRepository';
 
+// ver nas docs do typeorm-pagination
+interface IPagination {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 export class ListCustomersService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPagination> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customer = await customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    if (!customer) {
+    if (!customers) {
       throw new AppError('Customer not found');
     }
 
-    return customer;
+    return customers as IPagination;
   }
 }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { pagination } from 'typeorm-pagination';
 import { AppError } from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 import { errors } from 'celebrate';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
 
